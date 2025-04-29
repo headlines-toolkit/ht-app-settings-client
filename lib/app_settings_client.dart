4453956a07f0a025459b2565bb49700312291e07
fulleni <fulleni@hotmail.com>
2025-04-29 11:36:44 +0100

    chore(app_settings_client): add export libraries and models
    - Create app_settings_client library to define interface for managing application settings
    - Add models library to export all models and enums related to app settings
    - Include various app setting models such as theme, language, and font preferences

---
+++ lib/app_settings_client.dart
@@ -0,0 +1,6 @@
+/// A Dart package defining the interface for managing user-configurable
+/// application settings like theme, language, and font preferences.
+library app_settings_client;
+
+export 'src/app_settings_client.dart';
+export 'src/models/models.dart';
---
+++ lib/src/models/models.dart
@@ -0,0 +1,9 @@
+/// Barrel file for exporting all models and enums related to app settings.
+library models;
+
+export 'app_accent_theme.dart';
+export 'app_base_theme.dart';
+export 'app_font_weight.dart';
+export 'app_language.dart';
+export 'app_text_scale_factor.dart';
+export 'display_settings.dart';
